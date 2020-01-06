7da78e46f76bdef2665b29ce30d31fd0ca86bdf4
fastwave <cpaqyx@aliyun.com>
2020-01-06 13:36:48 +0800

    服务发现与注册课件上传

---
+++ coursewareV1.0/2.6Nacos集群/code/fastwave-service-auth/src/test/java/fastwave/cloud/fastwaveserviceauth/FastwaveServiceAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package fastwave.cloud.fastwaveserviceauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastwaveServiceAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
