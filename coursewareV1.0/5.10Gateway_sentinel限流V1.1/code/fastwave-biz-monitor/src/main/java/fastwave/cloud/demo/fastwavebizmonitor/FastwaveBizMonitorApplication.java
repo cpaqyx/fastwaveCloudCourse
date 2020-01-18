2df90761b8429296ab72112bec47acbba71e66de
fastwave <cpaqyx@aliyun.com>
2020-01-18 18:25:54 +0800

    更新网关sentinel课件

---
+++ coursewareV1.0/5.10Gateway_sentinel限流V1.1/code/fastwave-biz-monitor/src/main/java/fastwave/cloud/demo/fastwavebizmonitor/FastwaveBizMonitorApplication.java
@@ -0,0 +1,13 @@
+package fastwave.cloud.demo.fastwavebizmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastwaveBizMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastwaveBizMonitorApplication.class, args);
+    }
+
+}
---
+++ coursewareV1.0/5.10Gateway_sentinel限流V1.1/code/fastwave-service-gateway-v2/src/main/java/fastwave/cloud/demo/fastwaveservicegatewayv2/limit/SentinelResponse.java
@@ -0,0 +1,20 @@
+package fastwave.cloud.demo.fastwaveservicegatewayv2.limit;
+
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebExceptionHandler;
+import reactor.core.publisher.Mono;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+
+import java.nio.charset.StandardCharsets;
+
+public class SentinelResponse implements WebExceptionHandler {
+    @Override
+    public Mono<Void> handle(ServerWebExchange serverWebExchange, Throwable throwable) {
+        ServerHttpResponse serverHttpResponse = serverWebExchange.getResponse();
+        serverHttpResponse.getHeaders().add("Content-Type", "application/json;charset=UTF-8");
+        byte[] data = "{\"code\":403,\"message\":\"sentinel限流了\"}".getBytes(StandardCharsets.UTF_8);
+        DataBuffer buffer = serverHttpResponse.bufferFactory().wrap(data);
+        return serverHttpResponse.writeWith(Mono.just(buffer));
+    }
+}
