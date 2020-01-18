5db480950914c365b3893dd961709f9e90626448
fastwave <cpaqyx@aliyun.com>
2020-01-18 17:41:34 +0800

    更新sentinel熔断

---
+++ coursewareV1.0/3.9sentinel熔断/code/fastwave-service-gateway/src/main/java/fastwave/cloud/demo/fastwaveservicegateway/FastwaveServiceGatewayApplication.java
@@ -0,0 +1,30 @@
+package fastwave.cloud.demo.fastwaveservicegateway;
+
+import com.alibaba.cloud.sentinel.annotation.SentinelRestTemplate;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+@EnableFeignClients
+public class FastwaveServiceGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastwaveServiceGatewayApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    @SentinelRestTemplate
+    public RestTemplate restTemplate()
+    {
+        return new RestTemplate();
+    }
+}
